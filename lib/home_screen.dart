1a5d755208f4b8b00716be99055b96086ea7e5fc
Abu Anwar MD Abdullah <abuanwar072@gmail.com>
2025-06-17 18:32:00 +0600

    staring home update

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,27 +1,37 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final GlobalKey backgroundKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/images/sample_1.png',
-              fit: BoxFit.cover,
-            ),
-            Image.asset(
-              'assets/images/sample_2.jpg',
-              fit: BoxFit.cover,
-            ),
-            Image.asset(
-              'assets/images/sample_3.jpg',
-              fit: BoxFit.cover,
-            ),
-          ],
+      body: RepaintBoundary(
+        key: backgroundKey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/images/sample_1.png',
+                fit: BoxFit.cover,
+              ),
+              Image.asset(
+                'assets/images/sample_2.jpg',
+                fit: BoxFit.cover,
+              ),
+              Image.asset(
+                'assets/images/sample_3.jpg',
+                fit: BoxFit.cover,
+              ),
+            ],
+          ),
         ),
       ),
     );
